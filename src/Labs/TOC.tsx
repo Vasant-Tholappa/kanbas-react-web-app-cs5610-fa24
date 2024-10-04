3068fd87d4f3b914181ec5064ad21820993a5082
vasant <vasant.tholappa@gmail.com>
2024-10-04 01:44:22 -0400

    wd-github id added

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -35,7 +35,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/Vasant-Tholappa/kanbas-react-web-app-cs5610-fa24/tree/a2" className="nav-link">
+        <a id="wd-github" href="https://github.com/Vasant-Tholappa/kanbas-react-web-app-cs5610-fa24/tree/a2" className="nav-link">
           My GitHub
         </a>
       </li>
